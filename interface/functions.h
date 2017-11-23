10d662cd55f388b418a0c07880848106fcd96df0
Bruno Galinhas <bruno_galinhas@hotmail.com>
2017-11-23 23:57:39 +0000

    improved tables

--- interface/functions.h
+++ interface/functions.h
@@ -95,7 +95,7 @@ RooRealVar* branching_fraction(TString measure, int channel);
 //void read_vector(TString measure, int channel, TString vector, TString var1_name , TString var2_name, int n_var1_bins, int n_var2_bins,  double* var1_bins, double* var2_bins, double* array, double* err_lo = NULL, double* err_hi = NULL);
 void read_vector(int channel, TString vector, TString var1_name , TString var2_name, int n_var1_bins, int n_var2_bins,  double* var1_bins, double* var2_bins, double* array, double* err_lo = NULL, double* err_hi = NULL);
 void print_table(TString title, int n_var1_bins, int n_var2_bins, TString var1_name, TString var2_name, double* var1_bin_edges, double* var2_bin_edges, double* array, double* stat_err_lo, double* stat_err_hi, double* syst_err_lo = NULL, double* syst_err_hi = NULL, double* BF_err = NULL);
-void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::string> col_name, std::vector<std::string> labels, std::vector<std::vector<std::string> > numbers, std::string caption);
+void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::string> col_name, std::vector<std::string> labels, std::vector<std::vector<std::string> > numbers, std::string caption, bool vertical = false);
 
 void mc_study(RooWorkspace& w, int channel, double pt_min, double pt_max, double y_min, double y_max);
 
@@ -1399,7 +1399,7 @@ void print_table(TString title, int n_var1_bins, int n_var2_bins, TString var1_n
     }
 }
 
-void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::string> col_name, std::vector<std::string> labels, std::vector<std::vector<std::string> > numbers, std::string caption)
+void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::string> col_name, std::vector<std::string> labels, std::vector<std::vector<std::string> > numbers, std::string caption, bool vertical)
 {
   std::ofstream file;
 
@@ -1412,8 +1412,11 @@ void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::st
   file << "\\begin{table}" << std::endl;
   file << "\\centering" << std::endl;
   file << "\\caption{"+caption+"}" << std::endl;
-  //file << "\\begin{adjustbox}{width=1\\textwidth}" << std::endl;
-    
+  
+  if(vertical)
+    {
+      file << "\\begin{adjustbox}{angle=90,height=0.98\\textheight}" << std::endl;
+    }
   //setup table size                                                                                                                             
   TString col = "|c|";
 
@@ -1442,7 +1445,12 @@ void latex_table(std::string filename, int n_col, int n_lin, std::vector<std::st
 
   //End Table                                                                                                                                
   file << "\\end{tabular}" << std::endl;
-  //file << "\\end{adjustbox}" << std::endl;
+
+  if(vertical)
+    {
+      file << "\\end{adjustbox}" << std::endl;
+    }
+
   file << "\\end{table}" << std::endl;
 
   std::string line;

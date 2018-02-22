2b701b1bcd7e21b1c01a8c2b1310ff956298526e
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2018-02-22 18:20:58 +0000

    same as before

--- interface/functions.h
+++ interface/functions.h
@@ -1019,7 +1019,7 @@ RooRealVar* prefilter_efficiency(int channel, double pt_min, double pt_max, doub
 }
 
 //the weight are correct only for 'mu1pt', 'mu2pt' and 'pt'; for the other variables we still have to impose weight = 1 in some points
-RooRealVar* reco_efficiency(int channel, double pt_min, douvble pt_max, double y_min, double y_max, bool syst TString reweighting_var_str) 
+RooRealVar* reco_efficiency(int channel, double pt_min, double pt_max, double y_min, double y_max, bool syst, TString reweighting_var_str) 
 {
 
   double weight_passed = 0.;
@@ -1027,7 +1027,7 @@ RooRealVar* reco_efficiency(int channel, double pt_min, douvble pt_max, double y
   TH1D* h_weights_passed = nullptr;
 
   //------------read monte carlo gen without cuts-----------------------------
-  TString mc_input_no_cuts = TString::Format(BASE_DIR) + "/new_inputs/reduced_my_loop_gen_" + channel_to_ntuple_name(channel) + "_bmuonfilter.root";
+  TString mc_input_no_cuts = TString::Format(BASE_DIR) + "/new_inputs/reduced_myloop_gen_" + channel_to_ntuple_name(channel) + "_bmuonfilter.root";
   TFile *fin_no_cuts = new TFile(mc_input_no_cuts);
 
   TString ntuple_name = channel_to_ntuple_name(channel) + "_gen";
